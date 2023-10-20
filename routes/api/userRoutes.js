8819304fae015d88ea4c2074893f28eb381e6737
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-10-20 14:20:22 -0500

    Created file structure and added user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+// Controller functions for User endpoint.
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    createFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/userId/friends/:friendId').post(createFriend).delete(deleteFriend);
+
+module.exports = router;
